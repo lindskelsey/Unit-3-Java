1f755b8f01dc8a655e78d06f29e8531a5b7e9cfd
lindskelsey <lindsaykelsey@gmail.com>
2019-07-29 18:30:14 -0500

    add exercise 2 prep 3.2

--- src/exercises/ArrayListAndStrings.java
+++ src/exercises/ArrayListAndStrings.java
@@ -4,5 +4,29 @@
 
 package exercises;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ArrayListAndStrings {
+    public static void main(String[] args) {
+        //initialize arrayList
+        ArrayList<String> wordList = new ArrayList<>();
+        //add numbers to arrayList
+        Collections.addAll(wordList, "hello", "dog", "apple", "cat", "pencil");
+        //call sumOfEvens method with arrayList
+        printFives(wordList);
+
+    }
+
+    public static void printFives(ArrayList<String> wordList) {
+        //loop through every item in arrayList
+        for (int i = 0; i < wordList.size(); i++) {
+            //check if length of string at index is 5
+            if (wordList.get(i).length() == 5) {
+                //print out word
+                System.out.println(wordList.get(i));
+            }
+        }
+
+    }
 }
