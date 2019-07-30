64e655763de316e74f6982f7e41b15d2ac20870e
lindskelsey <lindsaykelsey@gmail.com>
2019-07-29 19:01:36 -0500

    add exercise 4 prep 3.2

--- src/exercises/HashMapStudentRoster.java
+++ src/exercises/HashMapStudentRoster.java
@@ -6,5 +6,42 @@
 
 package exercises;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class HashMapStudentRoster {
+    public static void main(String[] args) {
+
+        //initialize hashMap with int key and string value
+        HashMap<Integer, String> students = new HashMap<>();
+        Scanner in = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+
+        // Get student names and ids
+        do {
+
+            System.out.print("Student: ");
+            newStudent = in.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("Id Number: ");
+                int idNumber = in.nextInt();
+                students.put(idNumber, newStudent);
+
+                // Read in the newline before looping back
+                in.nextLine();
+            }
+
+        } while(!newStudent.equals(""));
+
+        // Print class roster
+        System.out.println("\nClass roster:");
+
+        for (Map.Entry<Integer, String> student : students.entrySet()) {
+            System.out.println(student.getKey() + " " + student.getValue());
+        }
+    }
 }
